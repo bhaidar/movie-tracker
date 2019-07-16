7d59d6f478a6e2d57853b993b6df9cff5a358f7f
Bilal Haidar <bhaidar@gmail.com>
2019-07-16 13:03:07 +0300

    Revert "add angular and nest apps (#1)"
    This reverts commit 4f8a2075b3a3526462adfd78b30d944dffaaa761.

--- apps/movietracker/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'movietracker',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/movietracker',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
