4f8a2075b3a3526462adfd78b30d944dffaaa761
Bilal Haidar <bhaidar@gmail.com>
2019-07-16 13:02:59 +0300

    add angular and nest apps (#1)
    * add angular and nest apps
    
    * cleanup code
    
    * fix pre-commit
    
    test husky
    
    test husky
    
    test husky again
    
    test husky also
    
    * install lint-staged
    
    * fix prettier
    
    * test tab and spacing
    
    * add all deps

---
+++ apps/api/src/app/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('todos')
+  getData() {
+    return this.appService.getData();
+  }
+}
